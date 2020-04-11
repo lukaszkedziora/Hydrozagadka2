4babf95ba11ebdce730bbabe2b244cf21389ff2e
lukaszkedziora <kontakt@lukaszkedziora.com>
2020-04-11 12:23:35 +0200

    PutChcractersOnBoard method added
    displaying characters from DB

--- View.cs
+++ View.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using SadConsole;
 using Console = SadConsole.Console;
+using Microsoft.Data.Sqlite;
 
 namespace Hydrozagadka2 {
 
@@ -15,11 +17,42 @@ namespace Hydrozagadka2 {
                 }
             }
         }
-    }
-    class TitleConsole : Console {
-        public TitleConsole (string title) : base (50, 50) {
-            Fill (Color.White, Color.Black, 176);
-            Print (0, 0, title.Align (HorizontalAlignment.Center, Width), Color.Black, Color.Yellow);
+
+        public static void PutCharactersOnBoard (Console console) {
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder ();
+
+            //Use DB in project directory.  If it does not exist, create it:
+            connectionStringBuilder.DataSource = "./Hydrozagadka2.db";
+
+            using (var connection = new SqliteConnection (connectionStringBuilder.ConnectionString)) {
+                connection.Open ();
+
+                // read characters (position and glyph) - As excluded
+                var selectCharacters = connection.CreateCommand ();
+                selectCharacters.CommandText = "SELECT * FROM Characters WHERE Name != 'As'";
+
+                using (var reader = selectCharacters.ExecuteReader ()) {
+
+                    while (reader.Read ()) {
+
+                        int positionX = Convert.ToInt32 ($"{reader.GetString(2)}");
+                        int positionY = Convert.ToInt32 ($"{reader.GetString(3)}");
+                        int glyph = Convert.ToInt32 ($"{reader.GetString(4)}");
+                        console.SetGlyph (positionX, positionY, glyph);
+
+                    }
+                }
+
+            }
+
+        }
+
+        class TitleConsole : Console {
+            public TitleConsole (string title) : base (50, 50) {
+                Fill (Color.White, Color.Black, 176);
+                Print (0, 0, title.Align (HorizontalAlignment.Center, Width), Color.Black, Color.Yellow);
+            }
         }
     }
 }
